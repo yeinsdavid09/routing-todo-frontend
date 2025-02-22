733029818337751b9e2488d023ab9086d2011b7d
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2025-02-21 21:06:41 -0500

    feat(tasks)/ can search taks using url

--- src/app/components/Search/Search.js
+++ src/app/components/Search/Search.js
@@ -1,4 +1,6 @@
-import { useContext } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+
 //* Base
 import style from "./Search.module.css";
 
@@ -8,7 +10,22 @@ import { TasksContext } from "../../contexts/tasksContext";
 export function Search() {
   //#region --------------------------------- Variables ---------------------------------
 
+  const [searchParams, setSearchParams] = useSearchParams();
   const { searchValue, setSearchValue } = useContext(TasksContext);
+
+  //#endregion
+
+  //#region --------------------------------- Methods ---------------------------------
+
+  useEffect(() => {
+    const search = searchParams.get("search") ?? "";
+    setSearchValue(search);
+  }, [searchParams, setSearchValue]);
+
+  function onSearchValueChange(value) {
+    setSearchValue(value);
+    setSearchParams({ search: value });
+  }
 
   //#endregion
 
@@ -28,7 +45,7 @@ export function Search() {
         type="text"
         placeholder="Escribe la tarea que deseas encontrar..."
         value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        onChange={(event) => onSearchValueChange(event.target.value)}
       ></input>
     </fieldset>
   );
